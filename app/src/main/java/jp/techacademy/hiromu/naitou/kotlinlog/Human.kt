afae21d6a12ed909e91862c9560ddfff7ce72b09
naitohiromu <naito.hiromu01@genetec.co.jp>
2023-05-17 09:25:05 +0900

    add thinkable

--- app/src/main/java/jp/techacademy/hiromu/naitou/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/hiromu/naitou/kotlinlog/Human.kt
@@ -2,12 +2,20 @@ package jp.techacademy.hiromu.naitou.kotlinlog
 
 import android.util.Log
 
-open class Human : Animal {
+open class Human : Animal,Thinkable {
     // 引数付きコンストラクタ
-    constructor(name: String, age: Int) : super(name,age) {
+    constructor(name: String, age: Int,hobby:String) : super(name,age,hobby) {
     }
+
     // メソッド
     override fun say() {
         Log.d("kotlintest",  "私の名前は" + this.name + "です。年は" + this.age + "歳です。")
     }
+    override fun think(){
+        Log.d("kotlintest","私は" + this.hobby + "について考える。")
+    }
+
+}
+interface Thinkable {
+    fun think()
 }
--- app/src/main/java/jp/techacademy/hiromu/naitou/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiromu/naitou/kotlinlog/MainActivity.kt
@@ -27,9 +27,13 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        val human = Human("ポチ", 3)      // 名前をポチ、年齢3歳で、Dogのインスタンスを作る
+        val human = Human("ポチ", 3,"散歩")      // 名前をポチ、年齢3歳で、Dogのインスタンスを作る
+        val human_t = Human("タロウ",10,"昼寝")
 
         human.say()
+        human.think()
+        human_t.say()
+        human_t.think()
     }
 }
 
